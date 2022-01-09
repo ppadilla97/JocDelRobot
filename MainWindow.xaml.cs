4461dd963132a6c30b2b8335580379b40e766b36
Pol Padilla <polpadillamorales@gmail.com>
2022-01-09 18:54:19 +0100

    Primera versió amb dues finestres

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JocRobot
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public int NumFiles
+        {
+            get
+            {
+                return Convert.ToInt32(tbFiles.Text);
+            }
+        }
+        public int NumColumnes
+        {
+            get
+            {
+                return Convert.ToInt32(tbColumnes.Text);
+            }
+        }
+
+        private void btnCreaTauler_Click(object sender, RoutedEventArgs e)
+        {
+            wndTauler finestra = new wndTauler();
+            finestra.Owner = this;
+            finestra.ShowDialog();
+        }
+    }
+}
